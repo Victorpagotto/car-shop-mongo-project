5522bb2176f3ae68e0c18584ccdc3c1e02e039bb
Victor Pagotto <victor.pagotto12@gmail.com>
2022-11-19 01:36:44 -0300

    Criados ODM's.

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,30 @@
+import { HydratedDocument, model, Model, models, Schema } from 'mongoose';
+
+export default abstract class AbstractODM<T> {
+  protected dbModel: Model<T>;
+  protected schema: Schema;
+  protected dbModelName: string;
+
+  constructor(schema: Schema, dbModelName: string) {
+    this.schema = schema;
+    this.dbModelName = dbModelName;
+    this.dbModel = models[this.dbModelName] || model(this.dbModelName, this.schema);
+  }
+
+  public async create(object: T): Promise<HydratedDocument<T>> {
+    return this.dbModel.create({ ...object });
+  }
+
+  public async findAll(): Promise<HydratedDocument<T>[]> {
+    return this.dbModel.find({});
+  }
+
+  public async destroy(id: string): Promise<boolean> {
+    const result = await this.dbModel.deleteOne({ id });
+    return !!result.deletedCount;
+  }
+
+  public async update(id: string, info: T): Promise<HydratedDocument<T> | null> {
+    return this.dbModel.findOneAndUpdate({ id }, { info }, { upsert: false });
+  }
+}
---
+++ src/Models/MotorcycleODM.ts
@@ -0,0 +1,20 @@
+import { Schema } from 'mongoose';
+import { IMotorcycle } from '../Interfaces/IMotorcycle';
+import AbstractODM from './AbstractODM';
+
+export default class MotorocycleODM extends AbstractODM<IMotorcycle> {
+  constructor() {
+    const modelSchema = new Schema<IMotorcycle>({
+      id: { type: String, required: true },
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: true },
+      buyValue: { type: Number, required: true },
+      category: { type: String, required: true },
+      engineCapacity: { type: Number, required: true },
+    });
+    const dbModelName = 'Car';
+    super(modelSchema, dbModelName);
+  }
+}
